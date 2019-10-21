03914174fd440a13e145d092a2a4cf25b2bf5cb2
Eric Manning <emanningmusic@yahoo.com>
2019-10-21 03:10:29 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,10 +2,8 @@
 def my_collect(array)
   i = 0 
   collection = []
-  while i < 
-  array.length
-  collection <<
-  yield(array[i])
+  while i < array.length
+  collection << yield(array[i])
   i += 1 
 return array
 end 
